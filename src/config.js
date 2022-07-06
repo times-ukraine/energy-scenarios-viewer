63ef4886043f616a02fd851978dddc0b0ea68a1c
Olexandr Balyk <olexandr.balyk@ucc.ie>
2022-07-06 13:33:30 -0500

    Fetch chartsInfo from study repository

--- src/config.js
+++ src/config.js
@@ -1,10 +1,11 @@
-import chartsInfo from "./specs/chartsInfo";
+import commonChartsInfo from "./specs/chartsInfo";
 import chartsTitles from "./specs/chartsTitles";
 import seriesTitles from "./specs/seriesTitles";
 import scenarioTitles from "./specs/scenarioTitles";
 import scenarios from "./specs/scenarios";
 import routes from "./specs/routes";
 import contentNavs from "./specs/contentNavs";
+import useFetch from "energy-charts/dist/hooks/useFetch";
 
 const chartsPath = "results/*";
 
@@ -22,10 +23,27 @@ const orgGhPages = "https://MaREI-EPMG.github.io";
 
 const periods = Array.from(Array(31), (e, i) => 2020 + i);
 
+let cache = {};
+
 const alert = {
   heading: "Work in progress...",
   text: "Please don't cite or use these results.",
   variant: "danger"
+};
+
+function ChartsInfo() {
+  const chartsInfo = useFetch(
+    `${orgGhPages}/tim-carbon-budgets-2022/specs/charts.json`,
+    cache
+  );
+
+  return chartsInfo;
+}
+
+const chartsInfo = {
+  "tim-carbon-budgets-2021": commonChartsInfo,
+  "tim-energy-security-2022": commonChartsInfo,
+  "tim-carbon-budgets-2022": ChartsInfo
 };
 
 let config = {};
@@ -34,7 +52,7 @@ studies.forEach(
   (study) =>
     (config[study] = {
       alert: alert,
-      chartsInfo: chartsInfo,
+      chartsInfo: chartsInfo[study],
       chartsPath: chartsPath,
       titles: {
         charts: chartsTitles,
